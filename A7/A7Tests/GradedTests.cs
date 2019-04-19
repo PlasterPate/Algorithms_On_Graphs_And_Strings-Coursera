381e94297cd0ad78af7ad7292f6748f911523214
AliMohammad <am.movahedian1998@gmail.com>
2019-04-19 17:21:30 +0000

    Updated GradedTests.cs

--- A7/A7Tests/GradedTests.cs
+++ A7/A7Tests/GradedTests.cs
@@ -25,7 +25,7 @@ namespace A7.Tests
             RunTest(new Q2CunstructSuffixArray("TD2"));
         }
 
-        [TestMethod()]
+        [TestMethod(), Timeout(1000)]
         public void SolveTest_Q3()
         {
             RunTest(new Q2CunstructSuffixArray("TD2"));
