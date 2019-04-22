852ca844b4944d79c1ae8f52267feae32e9e9033
AliMohammad Movahedian <am.movahedian1998@hgmail.com>
2019-04-22 20:21:39 +0430

    Exam1

---
+++ Exam1/Exam1/Q1Betweenness.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+
+namespace Exam1
+{
+    public class Q1Betweenness : Processor
+    {
+        public Q1Betweenness(string testDataName) : base(testDataName)
+        {
+            this.ExcludeTestCaseRangeInclusive(15, 50);
+        }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long[][], long[]>)Solve);
+
+
+        public long[] Solve(long NodeCount, long[][] edges)
+        {
+            Graph myGraph = new Graph(NodeCount, edges);
+            return myGraph.CalculateBetweenness();
+        }
+    }
+}
---
+++ Exam1/Exam1/Q2Cryptanalyst.cs
@@ -0,0 +1,55 @@
+﻿using TestCommon;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Exam1
+{
+    public class Q2Cryptanalyst : Processor
+    {
+        public Q2Cryptanalyst(string testDataName) : base(testDataName)
+        {
+            this.ExcludeTestCaseRangeInclusive(20, 37);
+            //this.ExcludeTestCaseRangeInclusive(27, 37);
+        }
+
+        public override string Process(string inStr) => Solve(inStr);
+
+        public HashSet<string> Vocab = new HashSet<string>();
+
+        public string Solve(string cipher)
+        {
+            var dict = File.ReadAllLines($"../../../dictionary.txt");
+            Encryption encryptor;
+            string key;
+            string text = string.Empty;
+            KMP kmp;
+            int wordsFound = 0;
+            bool flag = false;
+            
+            for (int i = 0; i < 100; i++)
+            {
+                wordsFound = 0;
+                key = i.ToString();
+                encryptor = new Encryption(key, ' ', 'z', false);
+                text = encryptor.Decrypt(cipher);
+                kmp = new KMP(text);
+                for (int j = 0; j < dict.Length; j++)
+                {
+                    if (dict[j].Length < 5)
+                        continue;
+                    wordsFound += kmp.Match(dict[j]);
+                    if (wordsFound > 10)
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag)
+                    break;
+            }
+            return text.GetHashCode().ToString();
+            
+        }
+    }
+}
